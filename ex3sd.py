36643051ac599cd6960f581190b2e478938c9507
jon-moreno <jonathan.moreno324@gmail.com>
2016-05-16 12:12:41 -0400

    Learning git the hard way. 'git commit -a' only affects deleted or modified files!

---
+++ ex3sd.py
@@ -0,0 +1,37 @@
+#Prints out the line, I will now count my chickens:
+print "I will now count my chickens:"
+
+#Prints out the string and then the calculation. Hens 30
+print "Hens", 25 + 30.0 / 6.0
+#Prints out the line, Roosters 97
+print "Roosters", 100 - 25 * 3.0 % 4.0
+
+#Prints out the line, Now I will count the eggs:
+print "Now I will count the eggs:"
+
+#Prints out the result of the calculation, 7
+print 3 + 2 + 1 - 5 + 4.0 % 2.0 - 1 / 4 + 6
+
+#Prints out the line, Is it true that 3 + 2 < 5 - 7?
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#Prints out the boolean value of the comparison, False
+print 3 + 2 < 5 - 7
+
+#Prints out the string then the calculation, What is 3 + 2? 5
+print "What is 3 + 2?", 3 + 2
+#Prints out the string then the calculation, What is 5 - 7? -2
+print "What is 5 - 7?", 5 - 7
+
+#Prints out the line, Oh that's why it's False.
+print "Oh that's why it's False."
+
+#Prints out the line, How about some more.
+print "How about some more."
+
+#Prints out the string then the result of the comparison, Is it greater? True
+print "Is it greater?", 5 > -2
+#Prints out the string then the result of the comparison, Is it greater or equal? True
+print "Is it greater or equal?", 5 >= -2
+#Prints out the string then the result of the comparison, Is it greater? False
+print "Is it less or equal?", 5 <= -2
